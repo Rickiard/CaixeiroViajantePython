fbf3f917d3c2092b153314d0195feb19968114e3
Rickiard <al79194@alunos.utad.pt>
2023-04-27 19:00:59 +0100

    Update Caixeiro Viajante.py

--- Caixeiro Viajante.py
+++ Caixeiro Viajante.py
@@ -1,7 +1,22 @@
 import math
 
+cidades = [(0, 0), (1, 2), (3, 1), (2, 4), (4, 3)]
+
+N = len(cidades) - 1
+
 def distance (location1, location2):
+
     x, y = location1
     a, b = location2
 
     return math.sqrt(pow(x - a, 2) + pow(y - b, 2))
+
+def CaixeiroViajante (cidades):
+    melhorRota = []
+    menorDistancia = 0
+
+    return (melhorRota, menorDistancia)
+
+melhorRota, menorDistancia = CaixeiroViajante(cidades)
+print("Melhor rota:", melhorRota)
+print("Menor distância:", menorDistancia)
