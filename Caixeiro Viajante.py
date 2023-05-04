1b0e602384aa2edcc2e6d5285725a8c11484fa38
Rickiard <131393767+Rickiard@users.noreply.github.com>
2023-05-04 19:09:36 +0100

    Update Caixeiro Viajante.py

--- Caixeiro Viajante.py
+++ Caixeiro Viajante.py
@@ -12,23 +12,24 @@ def distance (location1, location2):
     return math.sqrt(pow(x - a, 2) + pow(y - b, 2))
 
 def CaixeiroViajante (cidades):
-    melhorRota = []
-    menorDistancia = 0
+    melhorRota = [(0, 0)]
+    menorDistancia = 10000000000
     a = 0
     dist = 1000000000
-    for o in range(len(cidades))
-        for i in range(len(cidades))
-            if distance(cidades[a], cidades[i]) < dist:
-                dist = distance(cidades[a], cidades[i])
-                del melhorRota[-1]
-                melhorRota.append(cidades[i])
-                a = i
 
-        menorDistancia = min(dists)
+    for o in range(1, len(cidades)):
+        for i in range(len(cidades)):
+            if distance(cidades[a], cidades[i]) < dist and i != a:
+                dist = distance(cidades[a], cidades[i])
+
+                if dist < menorDistancia:
+                    melhorRota.append(cidades[i])
+                
+                if dist < menorDistancia:
+                    a = i
+        
+        if menorDistancia > dist:
+            menorDistancia = dist
     
     return (melhorRota, menorDistancia)
-
-melhorRota, menorDistancia = CaixeiroViajante(cidades)
-print("Melhor rota:", melhorRota)
-print("Menor distância:", menorDistancia)
 
