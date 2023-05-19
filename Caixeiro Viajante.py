11530af5f7ab38c4d2e719fb437bce9887c75001
edusilva1012 <131398028+edusilva1012@users.noreply.github.com>
2023-05-19 19:48:37 +0100

    Update Caixeiro Viajante.py

--- Caixeiro Viajante.py
+++ Caixeiro Viajante.py
@@ -3,32 +3,12 @@ import sys
 import os
 import platform
 
-cidades = [(0, 0), (1, 0), (3, 0), (10, 0), (2, 0)]
+cidades = []
 
-def menu():
-    op = int(input("\nCaixeiro Viajante:\n\n 1: Ler através de ficheiro\n 2: Ler através de teclado\n 3: Sair do programa\n\n"))
-    if platform.system() == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
-    if op == 1: 
-        print("1") 
-        menu()
-    elif op == 2: 
-        print("2")
-        
-        menu()
-    elif op == 3: sys.exit()
-    else: 
-        print("Opção inexistente! Tente novamente...")
-        input()
-        if platform.system() == 'Windows':
-            os.system('cls')
-        else:
-            os.system('clear')
-        menu()
-
-menu()
+if platform.system() == 'Windows':
+    os.system('cls')
+else:
+    os.system('clear')
 
 def distance (location1, location2):
 
@@ -38,8 +18,8 @@ def distance (location1, location2):
     return math.sqrt(pow(x - a, 2) + pow(y - b, 2))
 
 def CaixeiroViajante (cidades):
-    melhorRota = [(0, 0)]
-    menorDistancia = 10000000000
+    melhorRota = [cidades[0]]
+    menorDistancia = 0
     a = 1000000
     o = 0
     pos = 1
@@ -64,24 +44,58 @@ def CaixeiroViajante (cidades):
                         jaPassou.append(o)
                         o = i
 
-                if ant > a and a != 0:
-                    menorDistancia = a
-
-                ant = menorDistancia
-
         pos = pos + 1
-    
+    melhorRota.append(cidades[0])
+    for i in range(len(melhorRota) - 1):
+        menorDistancia = menorDistancia + distance(melhorRota[i], melhorRota[i+1])
     return (melhorRota, menorDistancia)
 
-melhorRota, menorDistancia = CaixeiroViajante(cidades)
-print("\nMelhor rota:", melhorRota)
-print("\nMenor distância:", menorDistancia, "\n")
+def ler_cidades_do_ficheiro(nome_ficheiro):
+    try:
+        with open(nome_ficheiro, 'r') as ficheiro:
+            for linha in ficheiro:
+                x, y = map(int, linha.split(','))
+                cidades.append((x, y))      
+            if platform.system() == 'Windows':
+                os.system('cls')
+            else:
+                os.system('clear')
+            print("Lista carregada:", cidades)
+            input()
+    except FileNotFoundError:
+        print("\nErro: Arquivo não encontrado. Tente novamente... ")
+        input()
+        if platform.system() == 'Windows':
+            os.system('cls')
+        else:
+            os.system('clear')
+        ler_cidades_do_ficheiro(nome_ficheiro = input("Introduza o nome do ficheiro: "))
 
-input()
+def ler_cidades_teclado():
+    N = int(input("Digite o número de cidades: "))
+    for i in range(N):
+        cidade = eval(input(f"\nQuais são as coordenadas da cidade {i+1} ('x,y'): "))
+        cidades.append(cidade)
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+    print("Lista carregada:", cidades)
+    input()
 
-if platform.system() == 'Windows':
-    os.system('cls')
-else:
-    os.system('clear')
+def printar(cidades):
+    melhorRota, menorDistancia = CaixeiroViajante(cidades)
+    print("Melhor rota:", melhorRota)
+    print("\nMenor distância:", menorDistancia, "\n")
+    input()
 
-menu()
+def menu():
+    op = int(input("Caixeiro Viajante:\n\n 1: Ler através de ficheiro\n 2: Ler através de teclado\n 3: Sair do programa\n\n"))
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+    if op == 1:
+        ler_cidades_do_ficheiro(nome_ficheiro = input("Introduza o nome do ficheiro: "))
+        printar(cidades)
+... (21 linhas)
